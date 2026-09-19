0e4301bad8fc4174ef38b359dec8d25ce24d423a
lucas augusto <luaugustomonteiror@gmail.com>
2026-09-18 21:59:40 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("--- Decomposição Decimal ---");
+
+
+Console.Write("Digite um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+
+int unidades = numero % 10;
+int restante = numero / 10;
+
+
+int dezenas = restante % 10;
+restante = restante / 10;
+
+
+int centenas = restante;
+
+
+Console.WriteLine($"O número {numero} possui:");
+Console.WriteLine($"Unidades: {unidades}");
+Console.WriteLine($"Dezenas: {dezenas}");
+Console.WriteLine($"Centenas: {centenas}");
